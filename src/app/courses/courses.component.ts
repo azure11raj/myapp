c6e0c43e079e4714330f4d8c4e485fe2f970ebcf
azure11raj <azureraj11@gmail.com>
2025-04-01 23:09:11 +0530

    content updated.

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterModule,Router } from '@angular/router';
 
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.css'
 })
